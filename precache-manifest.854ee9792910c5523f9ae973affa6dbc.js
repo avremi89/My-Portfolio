a943991f0561327b9a626c6b1fe5d6bc8625d3e5
avremi89 <avremi89@gmail.com>
2019-11-24 19:26:08 +0200

    Updates

---
+++ precache-manifest.854ee9792910c5523f9ae973affa6dbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e717e320d6ecb1920be656da2a1b36ea",
+    "url": "/portfolio4/index.html"
+  },
+  {
+    "revision": "f4c3979ade7a9fbebb7d",
+    "url": "/portfolio4/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "1a2af73f6f1311978233",
+    "url": "/portfolio4/static/js/2.4f67fa2e.chunk.js"
+  },
+  {
+    "revision": "f4c3979ade7a9fbebb7d",
+    "url": "/portfolio4/static/js/main.25e67b3d.chunk.js"
+  },
+  {
+    "revision": "621f17b880f014e4a680",
+    "url": "/portfolio4/static/js/runtime-main.70805bba.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/portfolio4/static/media/logo.25bf045c.svg"
+  }
+]);
